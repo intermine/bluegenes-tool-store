605af79ad2a56b70f764de968ee9d2aa465f9110
uosl <regen@anche.no>
2019-06-12 12:21:48 +0100

    Initial running implementation
    Basically code that is split out from BlueGenes.

---
+++ project.clj
@@ -0,0 +1,57 @@
+(defproject org.intermine/bluegenes-tool-store "0.0.1-SNAPSHOT"
+  :licence "LGPL-2.1-only"
+  :description "Microservice which serves tools for use with BlueGene's Tool API"
+  :url "http://www.intermine.org"
+  :dependencies [; Clojure
+                 [org.clojure/clojure "1.9.0"]
+
+                 ; HTTP
+                 [clj-http "3.7.0"]
+                 [compojure "1.6.0"]
+                 [ring "1.7.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.4.0" :exclusions [cheshire.core]]
+                 [cheshire "5.8.0"]
+                 [metosin/ring-http-response "0.9.1"]
+                 [ring-middleware-format "0.7.4"]
+
+                 ; Build tools
+                 [yogthos/config "0.9"]
+
+                 ; Logging
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.7"]]
+
+  :deploy-repositories {"clojars" {:sign-releases false}}
+  :plugins [[lein-codox "0.10.5"]
+            [lein-ancient "0.6.14"]
+            [lein-pdo "0.1.1"]
+            [lein-cljfmt "0.6.1"]]
+
+  :aliases {"dev" ["do" "clean" ["pdo" ["run"]]]
+            "prod" ["do" "clean" ["pdo" ["run"]]]
+            "format" ["cljfmt" "fix"]}
+
+  :min-lein-version "2.8.1"
+
+  :source-paths ["src/clj"]
+
+  :clean-targets ^{:protect false} ["target"]
+
+  :profiles {:dev {:resource-paths ["config/dev" "tools" "config/defaults"]}
+             :prod {:resource-paths ["config/prod" "tools"  "config/defaults"]}
+             :uberjar {:resource-paths ["config/prod" "config/defaults"]
+                       :prep-tasks ["clean" "compile"]
+                       :aot :all}
+             :java9 { :jvm-opts ["--add-modules" "java.xml.bind"]}}
+
+  :main bluegenes-tool-store.core
+
+  :uberjar-name "bluegenes-tool-store.jar"
+
+  :repositories [["clojars"
+                  {:url "https://clojars.org/repo"}]])
+                   ;; How often should this repository be checked for
+                   ;; snapshot updates? (:daily, :always, or :never)
+                   ;:update :always
+
---
+++ src/clj/bluegenes_tool_store/core.clj
@@ -0,0 +1,42 @@
+(ns bluegenes-tool-store.core
+  (:require [compojure.core :refer [GET defroutes context]]
+            [compojure.route :refer [resources files]]
+            [config.core :refer [env]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [bluegenes-tool-store.tools :refer [tools]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [taoensso.timbre :as timbre :refer [infof errorf]])
+  (:gen-class))
+
+(defroutes routes
+  ;; serve compiled files, i.e. js, css, from the resources folder
+  ;(resources "/")
+
+  ;; serve all tool files in bluegenes/tools automatically.
+  ;; they can't go in the resource folder b/c then they get jarred
+  ;; when running uberjar or clojar targets,
+  ;; and make the jars about a million megabytes too big.
+  (files "/tools" {:root (:bluegenes-tool-path env)})
+  (context "/api" []
+           (context "/tools" []
+                    (GET "/all" [] (tools)))))
+
+(def handler (-> #'routes
+                 ; Watch changes to the .clj and hot reload them
+                 wrap-reload
+                 ; Accept and parse request parameters in various formats
+                 (wrap-restful-format :formats
+                                      [:json :json-kw :transit-msgpack :transit-json])))
+
+(defn -main
+  "Start the BlueGenes Tool Store server.
+  This is the main entry point for the application"
+  [& args]
+  ;; Parse the port from the configuration file, environment variables, or default to 5001
+  ;; "PORT" is often the default value for app serving platforms such as Heroku and Dokku
+  (let [port (Integer/parseInt (or (:server-port env) (:port env) "5001"))]
+    (timbre/set-level! :info) ; Enable Logging
+    ;; Start the Jetty server by passing in the URL routes defined in `handler`
+    (run-jetty handler {:port port :join? false})
+    (infof "=== Bluegenes Tool Server server started on port: %s" port)))
