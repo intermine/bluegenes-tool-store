3dd5f549408bff3ab2e414c774de7da24d0d31c9
uosl <regen@anche.no>
2021-11-09 15:34:51 +0000

    Handle new config var and improve error handling/messages
    New config var is :bluegenes-backend-service-root

--- src/clj/bluegenes_tool_store/auth.clj
+++ src/clj/bluegenes_tool_store/auth.clj
@@ -2,48 +2,49 @@
   (:require [imcljs.auth :as im-auth]
             [cheshire.core :as cheshire]
             [ring.util.http-response :as response]
-            [config.core :refer [env]]))
+            [config.core :refer [env]]
+            [taoensso.timbre :as timbre :refer [error]]))
+
+(defn get-service-root [env]
+  (or (not-empty (:bluegenes-backend-service-root env))
+      (:bluegenes-default-service-root env)))
 
 (defn check-priv
   "Makes a request to the InterMine server to verify that the user in `req` is
   superuser on the mine which this BlueGenes instance belongs to. Only if this
   is true, will the `handler` function be run."
   [handler req]
-  (let [{:keys [token] :as service} (get-in req [:params :service])]
+  (let [{:keys [token] :as service} (get-in req [:params :service])
+        mine-name (get-in req [:params :mine-name])
+        ;; Doesn't care what mine you're on - will always verify with the default service.
+        service (assoc service :root (get-service-root env))]
     (try
-      (let [res      (im-auth/who-am-i? service token)
-            env-root (:bluegenes-default-service-root env)]
+      (let [res (im-auth/who-am-i? service token)]
         (cond
-          (and (:superuser res)
-               ;; Make sure that this BlueGenes instance belongs to the
-               ;; service which we query for the superuser flag.
-               (= (:root service) env-root))
-               ;; A different way to do this would be to replace the root
-               ;; key in the service to be env-root for the who-am-i?.
+          (:superuser res)
           (handler req)
 
           (contains? res :superuser)
           (response/unauthorized
-            {:error
-             (if env-root
-               (str "You need to be a superuser on '" env-root "' to use this API")
-               "The default service root for this BlueGenes instance needs to be defined")})
+            {:error (str "You need to login as a superuser on '" mine-name "' to perform tool operations on this BlueGenes.")})
 
           :else
           ;; The InterMine instance is too old to support returning the
           ;; superuser flag. We will instead give the user a useful message.
           (response/not-implemented
             {:error "Managing BlueGenes tools automatically is only supported on InterMine version 4.2.0 or newer."})))
+      (catch java.net.ConnectException _e
+        ;; Request timed out.
+        (response/not-found
+          {:error (str "Unable to reach InterMine service '" (:bluegenes-default-service-root env) "' to check privileges.")}))
       (catch Exception e
-        (let [{:keys [status body] :as error} (ex-data e)]
-          ;; Parse the body of the bad request sent back from the IM server.
-          (let [json-response (cheshire/parse-string body)]
-            (case status
-              401 (response/unauthorized json-response)
-              500 (response/internal-server-error json-response)
-              (response/not-found {:stack-trace error
-                                   :error
-                                   (str "Unable to reach InterMine service '"
-                                        (:root service)
-                                        "' to check privileges")}))))))))
-
+        (let [{:keys [status] :as res} (ex-data e)]
+          (case status
+            ;; User likely passed the token for a different mine.
+            (401 403) (response/unauthorized
+                        {:error (str "You need to login as a superuser on '" mine-name "' to perform tool operations on this BlueGenes.")})
+            ;; Internal error thrown by one of our functions.
+            nil (response/internal-server-error
+                  {:error (str "Server error: " (ex-message e))})
+            ;; Catch-all for unexpected error. Log and forward the erroneous response.
+            (do (error e) res)))))))
